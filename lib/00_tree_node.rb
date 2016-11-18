8e328bdc4363eaf4098a66e873bed834562623fc
App Academy Student <student@appacademy.io>
2016-11-18 11:12:13 -0800

    Add parent= method to PolyTreeNOde class

---
+++ lib/00_tree_node.rb
@@ -0,0 +1,42 @@
+class PolyTreeNode
+  attr_accessor :children
+
+  def initialize(value)
+    @parent = nil
+    @value = value
+    @children = []
+
+  end
+
+  def parent
+    @parent
+  end
+
+
+  def value
+    @value
+  end
+
+  def parent=(mother)
+    if @parent && @parent.children.include?(self)
+      @parent.children.delete(self)
+    end
+    @parent = mother
+    mother.children << self unless mother.nil?
+  end
+
+  def add_child(child)
+    child.parent = self
+  end
+
+
+
+
+end
+
+if __FILE__ == $PROGRAM_NAME
+  n = PolyTreeNode.new(1)
+  n2 = PolyTreeNode.new(2)
+  n2.parent = n
+  p n
+end
