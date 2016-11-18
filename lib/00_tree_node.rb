642559dd5b795db6ef62e8d21ceea5028dfaf428
App Academy Student <student@appacademy.io>
2016-11-18 11:52:57 -0800

    Add bfs method to PolyTreeNode

--- lib/00_tree_node.rb
+++ lib/00_tree_node.rb
@@ -41,7 +41,19 @@ class PolyTreeNode
     return self if @value == target_val
     @children.each do |child|
       child_result = child.dfs(target_val)
-      return child_result if child_result 
+      return child_result if child_result
+    end
+    nil
+  end
+
+  def bfs(target_val)
+    queue = [self]
+    until queue.empty?
+      curr_el = queue.shift
+      return curr_el if curr_el.value == target_val
+      curr_el.children.each do |child|
+        queue << child
+      end
     end
     nil
   end
@@ -52,5 +64,7 @@ if __FILE__ == $PROGRAM_NAME
   n = PolyTreeNode.new(1)
   n2 = PolyTreeNode.new(2)
   n2.parent = n
-  p n
+  n3 = PolyTreeNode.new(3)
+  n3.parent = n
+  p n.bfs(3)
 end
